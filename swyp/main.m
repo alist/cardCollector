8e623b3468d17309281db2410c08d9e9d4ed30e3
Alexander List <alist@listprojects.com>
2012-02-03 14:09:13 -0500

    Initial Commit

---
+++ swyp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  swyp
+//
+//  Created by Alexander List on 2/3/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISAppDelegate class]));
+	}
+}
